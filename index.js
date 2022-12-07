181bea81661e3c29415907448179ca77c8f46f97
Maghawry <maghawry.hussein20@gmail.com>
2022-12-07 08:45:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let button = document.querySelector(".get-color")
+let container = document.querySelector("body")
+let colorSpan = document.querySelector(".color")
+button.addEventListener("click", () => {
+  let index = Math.ceil(Math.random() * 29900) //* To get a random index of 29900 array of colors.
+  fetch("https://api.color.pizza/v1/")
+    .then((res) => res.json())
+    .then((data) => {
+      let color = data.colors[index].hex
+      colorSpan.textContent = color.toUpperCase()
+      container.style.backgroundColor = String(color)
+      button.style.backgroundColor = color
+    })
+})
