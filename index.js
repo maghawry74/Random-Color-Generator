d93f167bfc8b54204e96dc76900a129a9c980762
Maghawry <maghawry.hussein20@gmail.com>
2022-12-07 11:45:58 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ let button = document.querySelector(".get-color")
 let container = document.querySelector("body")
 let colorSpan = document.querySelector(".color")
 let toast = document.querySelector(".toast")
+let color = "#F1F5F8"
 button.addEventListener("click", () => {
   let index = Math.ceil(Math.random() * 29900) //* To get a random index of 29900 array of colors.
-  let time
-  clearInterval(time)
   fetch("https://api.color.pizza/v1/")
     .then((res) => res.json())
     .then((data) => {
-      let color = data.colors[index].hex
+      color = data.colors[index].hex
       colorSpan.textContent = color.toUpperCase()
       container.style.backgroundColor = String(color)
       button.style.backgroundColor = color
-      navigator.clipboard.writeText(color)
-      toast.style.opacity = "1"
-      toast.style.backgroundColor = color
-      time = setTimeout(() => {
-        toast.style.opacity = "0"
-      }, 3000)
     })
+})
+colorSpan.addEventListener("click", () => {
+  let time
+  clearInterval(time)
+  navigator.clipboard.writeText(color)
+  toast.style.opacity = "1"
+  time = setTimeout(() => {
+    toast.style.opacity = "0"
+  }, 1500)
 })
